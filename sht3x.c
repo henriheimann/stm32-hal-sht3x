61e8e8f6b7a36b56ef54360f75e1868fd1d1648b
Henri Heimann <henriheimann@gmail.com>
2020-10-23 19:02:02 +0200

    Implement basic sensor functionallity

---
+++ sht3x.c
@@ -0,0 +1,103 @@
+#include "sht3x.h"
+
+#include <assert.h>
+
+/**
+ * Registers addresses.
+ */
+typedef enum
+{
+	SHT3X_COMMAND_MEASURE_HIGHREP_STRETCH = 0x2c06,
+	SHT3X_COMMAND_HEATER_ENABLE = 0x306d,
+	SHT3X_COMMAND_HEATER_DISABLE = 0x3066,
+	SHT3X_COMMAND_READ_STATUS = 0xf32d
+} sht3x_command_t;
+
+static uint8_t calculate_crc(const uint8_t *data, size_t length)
+{
+	uint8_t crc = 0xff;
+	for (size_t i = 0; i < length; i++) {
+		crc ^= data[i];
+		for (size_t j = 0; j < 8; j++) {
+			if ((crc & 0x80u) != 0) {
+				crc = (uint8_t)((uint8_t)(crc << 1u) ^ 0x31u);
+			} else {
+				crc <<= 1u;
+			}
+		}
+	}
+	return crc;
+}
+
+static bool sht3x_send_command(sht3x_handle_t *handle, sht3x_command_t command)
+{
+	uint8_t command_buffer[2] = {(command & 0xff00u) >> 8u, command & 0xffu};
+
+	if (HAL_I2C_Master_Transmit(handle->i2c_handle, handle->device_address << 1u, command_buffer, sizeof(command_buffer),
+	                            SHT3X_I2C_TIMEOUT) != HAL_OK) {
+		return false;
+	}
+
+	return true;
+}
+
+static uint16_t uint8_to_uint16(uint8_t msb, uint8_t lsb)
+{
+	return (uint16_t)((uint16_t)msb << 8u) | lsb;
+}
+
+bool sht3x_init(sht3x_handle_t *handle)
+{
+	assert(handle->i2c_handle->Init.NoStretchMode == I2C_NOSTRETCH_DISABLE);
+	// TODO: Assert i2c frequency is not too high
+
+	uint8_t status_reg_and_checksum[3];
+	if (HAL_I2C_Mem_Read(handle->i2c_handle, handle->device_address << 1u, SHT3X_COMMAND_READ_STATUS, 2, (uint8_t*)&status_reg_and_checksum,
+					  sizeof(status_reg_and_checksum), SHT3X_I2C_TIMEOUT) != HAL_OK) {
+		return false;
+	}
+
+	uint8_t calculated_crc = calculate_crc(status_reg_and_checksum, 2);
+
+	if (calculated_crc != status_reg_and_checksum[2]) {
+		return false;
+	}
+
+	return true;
+}
+
+bool sht3x_read_temperature_and_humidity(sht3x_handle_t *handle, float *temperature, float *humidity)
+{
+	sht3x_send_command(handle, SHT3X_COMMAND_MEASURE_HIGHREP_STRETCH);
+
+	HAL_Delay(1);
+
+	uint8_t buffer[6];
+	if (HAL_I2C_Master_Receive(handle->i2c_handle, handle->device_address << 1u, buffer, sizeof(buffer), SHT3X_I2C_TIMEOUT) != HAL_OK) {
+		return false;
+	}
+
+	uint8_t temperature_crc = calculate_crc(buffer, 2);
+	uint8_t humidity_crc = calculate_crc(buffer + 3, 2);
+	if (temperature_crc != buffer[2] || humidity_crc != buffer[5]) {
+		return false;
+	}
+
+	int16_t temperature_raw = (int16_t)uint8_to_uint16(buffer[0], buffer[1]);
+	uint16_t humidity_raw = uint8_to_uint16(buffer[3], buffer[4]);
+
+	*temperature = -45.0f + 175.0f * (float)temperature_raw / 65535.0f;
+	*humidity = 100.0f * (float)humidity_raw / 65535.0f;
+
+	return true;
+}
+
+bool sht3x_set_header_enable(sht3x_handle_t *handle, bool enable)
+{
+	if (enable) {
+		return sht3x_send_command(handle, SHT3X_COMMAND_HEATER_ENABLE);
+	} else {
+		return sht3x_send_command(handle, SHT3X_COMMAND_HEATER_DISABLE);
+	}
+}
+
---
+++ sht3x.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <stdbool.h>
+
+#if (defined STM32L011xx) || (defined STM32L021xx) || \
+	(defined STM32L031xx) || (defined STM32L041xx) || \
+	(defined STM32L051xx) || (defined STM32L052xx) || (defined STM32L053xx) || \
+	(defined STM32L061xx) || (defined STM32L062xx) || (defined STM32L063xx) || \
+	(defined STM32L071xx) || (defined STM32L072xx) || (defined STM32L073xx) || \
+	(defined STM32L081xx) || (defined STM32L082xx) || (defined STM32L083xx)
+#include "stm32l0xx_hal.h"
+#elif defined (STM32L412xx) || defined (STM32L422xx) || \
+	defined (STM32L431xx) || (defined STM32L432xx) || defined (STM32L433xx) || defined (STM32L442xx) || defined (STM32L443xx) || \
+	defined (STM32L451xx) || defined (STM32L452xx) || defined (STM32L462xx) || \
+	defined (STM32L471xx) || defined (STM32L475xx) || defined (STM32L476xx) || defined (STM32L485xx) || defined (STM32L486xx) || \
+    defined (STM32L496xx) || defined (STM32L4A6xx) || \
+    defined (STM32L4R5xx) || defined (STM32L4R7xx) || defined (STM32L4R9xx) || defined (STM32L4S5xx) || defined (STM32L4S7xx) || defined (STM32L4S9xx)
+#include "stm32l4xx_hal.h"
+#else
+#error Platform not implemented
+#endif
+
+#ifndef SHT3X_I2C_TIMEOUT
+#define SHT3X_I2C_TIMEOUT 30
+#endif
+
+#define SHT3X_I2C_DEVICE_ADDRESS_ADDR_PIN_LOW 0x44
+#define SHT3X_I2C_DEVICE_ADDRESS_ADDR_PIN_HIGH 0x45
+
+/**
+ * Structure defining a handle describing a SHT3x device.
+ */
+typedef struct {
+
+	/**
+	 * The handle to the I2C bus for the device.
+	 */
+	I2C_HandleTypeDef *i2c_handle;
+
+	/**
+	 * The I2C device address.
+	 * @see{PCA9865_I2C_DEVICE_ADDRESS_ADDR_PIN_LOW}
+	 */
+	uint16_t device_address;
+
+} sht3x_handle_t;
+
+/**
+ * Initialises an SHT3x device.
+ * @param handle Handle to a SHT3x device.
+ * @return True on success, false otherwise.
+ */
+bool sht3x_init(sht3x_handle_t *handle);
+
+bool sht3x_read_temperature_and_humidity(sht3x_handle_t *handle, float *temperature, float *humidity);
+
+bool sht3x_set_header_enable(sht3x_handle_t *handle, bool enable);
